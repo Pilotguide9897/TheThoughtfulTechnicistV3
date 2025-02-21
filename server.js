7f4d475f96a2a2758143c5ae9eafca58bbe5ca0a
Jared Green <jaredryan1997@hotmail.com>
2025-02-21 13:30:26 -0400

    Minimal Setup Test

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const helpers = require("./utils/helpers");
 const session = require("express-session");
+const cors = require("cors");
 
 // create an instance of the express app and define the port.
 const app = express();
@@ -28,16 +29,26 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(cors());
+
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
-app.use(routes);
+// const hbs = exphbs.create({ helpers });
+// app.engine("handlebars", hbs.engine);
+// app.set("view engine", "handlebars");
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, "public")));
+// app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+// });
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
