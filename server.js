3437b9fd521b905c21cfc332f7e1a55c73606c5e
Jared Green <jaredryan1997@hotmail.com>
2025-02-21 13:37:36 -0400

    Updated server

--- server.js
+++ server.js
@@ -33,22 +33,22 @@ app.use(cors());
 
 app.use(session(sess));
 
-// const hbs = exphbs.create({ helpers });
-// app.engine("handlebars", hbs.engine);
-// app.set("view engine", "handlebars");
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, "public")));
-// app.use(routes);
+const hbs = exphbs.create({ helpers });
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
 
-// sequelize.sync({ force: false }).then(() => {
-//   app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
 // });
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server is running on port 3000");
+// });
